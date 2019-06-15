6c0abe8b8d5bf73fdfedaebbfb8a0b2a7100db7b
zhangsan <123456>
2019-06-15 18:33:11 +0800

    first commit...2

---
+++ git_test_pro/src/cn/itcast/FirstClass.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+        System.out.println("FirstClass ...");
+    }
+}
