3776b7fcb1804054b5a8cc16c846d36120412732
Itheima_YM <Itheima_YM@itcast.cn>
2019-06-16 23:11:22 +0800

    first commit test_git_prj

---
+++ test_git_prj/src/com/itheima/TestMain.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+        System.out.println("sssssssssssssss");
+    }
+}
